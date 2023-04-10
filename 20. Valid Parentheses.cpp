774696f63c9dd2f3ffd43c54b784333e18f4f245
withPain <krushnalokare97991@gmail.com>
2023-04-10 10:17:10 +0530

    Valid Parentheses

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> c;
+        if(s.length() == 1){}
+        for(int i =0 ;i<s.length();i++){
+            char ch = s[i];
+            //opening bracket
+            if(ch == '(' || ch == '{' || ch == '['){
+                c.push(ch);
+            }
+            //closing bracket
+            else{
+                if(!c.empty()){
+                    char top = c.top();
+                    if( (ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[') ){
+                        c.pop();
+                    }
+                    // s = '(])'
+                    else
+                    return false;
+                }
+                //s = ')'
+                else
+                return false;
+            }
+        }
+
+        if(c.empty())
+        return true;
+        else
+        return false;
+    }
+};
